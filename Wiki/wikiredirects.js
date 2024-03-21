7fe8c41575fc395cf256ecca5a2199e3f210811d
Justindraak1 <justinmonne@hotmail.com>
2024-03-21 16:50:36 +0100

    Made js file for all redirect funtions to be called from and defined in

---
+++ Wiki/wikiredirects.js
@@ -0,0 +1,17 @@
+function RedirectToWikiMainPage() {
+    window.location.href = '/Wiki/wiki.html'
+}
+
+/*Redirects to Categories & their subcategories*/
+function RedirectToDragons() {
+    window.location.href = '/Wiki/Dragons/dragons.html';
+
+}
+function RedirectToBoulderClass() {
+    window.location.href = '/Wiki/Dragons/Boulder/class.html'
+}
+
+/*Redirects to Dragon pages*/
+function RedirectToGronckle() {
+    window.location.href = 'Dragons/Boulder/Gronckle/gronckle.html';
+}
