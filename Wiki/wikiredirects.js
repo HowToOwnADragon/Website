9ad27257d6326ba164ea052d9a154d104d0f8f60
Justindraak1 <justinmonne@hotmail.com>
2024-03-25 21:17:31 +0100

    Added working redirects to every page that needs them

--- Wiki/wikiredirects.js
+++ Wiki/wikiredirects.js
@@ -7,9 +7,27 @@ function RedirectToDragons() {
     window.location.href = '/Wiki/Dragons/dragons.html';
 
 }
-function RedirectToBoulderClass() {
-    window.location.href = '/Wiki/Dragons/Boulder/class.html'
-}
+    function RedirectToBoulderClass() {
+        window.location.href = '/Wiki/Dragons/Boulder/class.html'
+    }
+    function RedirectToMysteryClass() {
+        window.location.href = '/Wiki/Dragons/Mystery/class.html'
+    }
+    function RedirectToSharpClass() {
+        window.location.href = '/Wiki/Dragons/Sharp/class.html' 
+    }
+    function RedirectToStokerClass() {
+        window.location.href = '/Wiki/Dragons/Stoker/class.html' 
+    }
+    function RedirectToStrikeClass() {
+        window.location.href = '/Wiki/Dragons/Sharp/class.html' 
+    }
+    function RedirectToTidalClass() {
+        window.location.href = '/Wiki/Dragons/Tidal/class.html' 
+    }
+    function RedirectToTrackerClass() {
+        window.location.href = '/Wiki/Dragons/Tracker/class.html' 
+    }
 
 /*Redirects to Dragon pages*/
 function RedirectToGronckle() {
